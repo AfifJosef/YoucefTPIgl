f496c4b7599f0bdc72f18f1dbfbb88569874f84a
Youcef <afifbensid@hotmail.com>
2017-10-17 20:30:37 +0100

    modifier l exception

--- Fichiers(Youcef)/src/VectorHelper.java
+++ Fichiers(Youcef)/src/VectorHelper.java
@@ -1,19 +1,8 @@
-/**
- * 
- * @author youcef
- *
- */
 
 
 public class VectorHelper 
 {
-	/**
-	 * 
-	 * @param tab1 le premier tableau pour la somme
-	 * @param tab2 le deuxiemme
-	 * @return un tableau de la meme taille que les tab1 et tab2 
-	 * @throws TailleException si la taille de tab1 differe de la taille de tab2
-	 */
+	
 	
     public static int [] SommeVec (int tab1[],int tab2[]) throws TailleException
     {
@@ -28,15 +17,10 @@ public class VectorHelper
     	}
     	else
     	{
-    		//throw TailleException
+    		Exception e = new TailleException();
     	}
     	return resultat;
     }
-    /**
-     * 
-     * @param tab un tableau d'entiers
-     * @return un tableau de deux cases: la premiere c'est le min, la deuxieme c'est le max
-     */
     
     
     public static int [] MinMax (int tab[])
