9a6dcb1463bf6085fdbb8c7f2e62fe02f69e1329
MouloudY <32851413+MouloudY@users.noreply.github.com>
2017-10-20 11:54:38 +0100

    Add files via upload

--- Fichiers(Youcef)/src/VectorHelperTest.java
+++ Fichiers(Youcef)/src/VectorHelperTest.java
@@ -1,29 +1,48 @@
-import static org.junit.Assert.*;
+package tpIgl;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
 /**
- * Une classe pour tester nos methodes
- * @author youcef bensid
+ * Tests Unitaires
+ * Cette classe a pour objectif de tester les méthodes de la classe VectorHelper
+ * @author Mouloud
+ * @since 2.0
  *
  */
+
+import org.junit.Test;
+
+import tpIgl.VectorHelper;
 
 public class VectorHelperTest {
 
 	@Test
-	public void testSommeVec() throws TailleException {
-		int tab1[] = {1,1,6};
-		int tab2[] = {2,1,1,5};
-		int resultat[] = {3,2,7};
-		assertArrayEquals(VectorHelper.SommeVec(tab1,tab2),null);
-	
+	public void testSortTable() {
+		int[] tableau={6,1,-100,1,18,13};
+		int[] resultatAttendu={-100,1,1,6,13,18};
+		int[] resultatObtenu;
+		resultatObtenu=VectorHelper.sortTable(tableau);
+		assertArrayEquals(resultatAttendu,resultatObtenu);
+		
 	}
 
 	@Test
-	public void testMinMax() 
-	{
-		int tab [] = {1,5,6,3};
-		int resultat [] = {1,6};
-		assertArrayEquals(VectorHelper.MinMax(tab),resultat);
+	public void testReverseTable() {
+		int[] tableau={6,1,-100,18,13};
+		int[] resultatAttendu={13,18,-100,1,6};
+		int[] resultatObtenu;
+		resultatObtenu= VectorHelper.reverseTable(tableau);
+		assertArrayEquals(resultatAttendu,resultatObtenu);
+		
+	}
+
+	@Test
+	public void testFonctionVector() {
+		int[] tableau = {4,9,10,17,5,19};
+		int[] resultatAttendu = {1,0,1,2,2,1};
+		int[] resultatObtenu;
+		int n=3;
+		resultatObtenu= VectorHelper.fonctionVector(tableau, n);
+		assertArrayEquals(resultatAttendu,resultatObtenu);
 		
 	}
 
